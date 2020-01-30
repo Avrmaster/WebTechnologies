aa6408dd58f801571cc25c9a8bddf378352f1940
Oleksandr Leskiv <oleksandr.leskiv@digia.co>
2020-01-30 11:14:07 +0200

    lab06 - 1.

--- js/index.js
+++ js/index.js
@@ -1,26 +1,20 @@
-function findWords(source, search) {
+function wordIndex(source, n) {
+	let currentWordInd = -1
 	for (let i = 0; i < source.length; ++i) {
-		if (checkMatch(source, i, search)) {
-			return true
+		if (
+			(source[i - 1] === undefined || source[i - 1] === ' ') &&
+			(source[i] !== ' ')
+		) {
+			if (++currentWordInd === n) {
+				return i
+			}
 		}
 	}
-
-	return false
+	return -1
 }
 
-function checkMatch(
-	source, indSrc, search,
-) {
-	for (let i = 0; i < search.length; ++i) {
-		if (source[indSrc + i] !== search[i]) {
-			return false
-		}
-	}
-	return true
-}
-
-console.log(findWords('Hello World Baa', 'World'))
-console.log(findWords('Hello World Baa', 'Hell o'))
-console.log(findWords('Hello World Baa', 'd B'))
-console.log(findWords('Hello World Baa', 'd Bb'))
-console.log(findWords('Hello World Baa', 'Baa'))
+console.log(wordIndex(' Hello World Baa  Ghj', 0))
+console.log(wordIndex(' Hello World Baa  Ghj', 1))
+console.log(wordIndex(' Hello World Baa  Ghj', 2))
+console.log(wordIndex(' Hello World Baa  Ghj', 3))
+console.log(wordIndex(' Hello World Baa  Ghj', 4))
